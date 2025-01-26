005bb82bd4925f0c43d1edcc6c9986fd2a4e1cc0
Nic <101201789+nic5694@users.noreply.github.com>
2025-01-26 10:50:46 -0500

    Update Header.tsx

--- src/pages/userFormPage/Header.tsx
+++ src/pages/userFormPage/Header.tsx
@@ -6,7 +6,7 @@ import styles from './Header.module.css';
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>Luigi.AI</h1>
+      <h1 className={styles.title}>Resume Wrecker</h1>
     </header>
   );
 };
